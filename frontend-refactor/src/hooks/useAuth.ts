91a4e9c5c5f4847ae12372245df8090fbc2876e5
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2023-10-23 09:34:15 -0300

    feat: add useAuth hook

---
+++ frontend-refactor/src/hooks/useAuth.ts
@@ -0,0 +1,49 @@
+import { HttpResponse } from '@/@types/HttpResponse'
+import { SignIn } from '@/pages/auth/sign-in/sign-in.schema'
+import { SignUp } from '@/pages/auth/sign-up/sign-up.schema'
+import { frontend } from '@/routes/routes'
+import { api } from '@/services/api'
+import { useMutation } from 'react-query'
+import { useNavigate } from 'react-router-dom'
+import { useMutate } from './useMutate'
+
+type HttpSignInResponse = HttpResponse & {
+  token: string
+}
+
+export const useAuth = () => {
+  const navigate = useNavigate()
+  const { promise } = useMutate()
+
+  const signIn = useMutation(
+    async (data: SignIn) => {
+      const url = `/auth/sign-in`
+      return await promise(api.post(url, data))
+    },
+    {
+      onSuccess: (response: HttpSignInResponse) => {
+        localStorage.setItem('token', response.token)
+        navigate(frontend.workspaces.index)
+      }
+    }
+  )
+
+  const signOut = () => {
+    localStorage.removeItem('token')
+    navigate(frontend.auth.sign_in.index)
+  }
+
+  const signUp = useMutation(
+    async (data: SignUp) => {
+      const url = `/auth/sign-up`
+      return await promise(api.post(url, data))
+    },
+    {
+      onSuccess: () => {
+        navigate(frontend.auth.sign_in.index)
+      }
+    }
+  )
+
+  return { signIn, signOut, signUp }
+}
