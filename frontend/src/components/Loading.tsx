38c8033e781fab307b756a8778db2c83c0db907c
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2023-10-23 11:06:51 -0300

    feat: add initial loading

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -2,7 +2,7 @@ import { CircularProgress, CircularProgressProps } from '@nextui-org/react'
 
 export const Loading = ({ ...props }: CircularProgressProps) => {
   return (
-    <div className="w-full h-[calc(100vh-6rem)] flex items-center justify-center">
+    <div className="w-full h-screen flex items-center justify-center">
       <CircularProgress aria-label="Loading..." {...props} />
     </div>
   )
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -21,7 +21,7 @@ export const App = () => {
     <React.StrictMode>
       <HelmetProvider>
         <QueryClientProvider client={queryClient}>
-          <Suspense fallback={<Loading />}>
+          <Suspense fallback={<Loading color="default" />}>
             <RouterProvider router={router} />
           </Suspense>
         </QueryClientProvider>
