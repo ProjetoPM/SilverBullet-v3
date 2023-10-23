2481728aa3f05e9a3921693c4090dfde8a1e3777
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2023-10-23 09:37:09 -0300

    feat: add AuthGuard and AuthGuest

---
+++ frontend-refactor/src/utils/guard/AuthGuard.tsx
@@ -0,0 +1,22 @@
+import { frontend } from '@/routes/routes'
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+type AuthGuardProps = {
+  children: React.ReactNode
+}
+
+export const AuthGuard = ({ children }: AuthGuardProps) => {
+  const [mounted, setMounted] = useState(false)
+  const token = localStorage.getItem('token')
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!token) {
+      navigate(frontend.auth.sign_in.index)
+    }
+    setMounted(true)
+  }, [token, navigate])
+
+  return <>{mounted && children}</>
+}
---
+++ frontend-refactor/src/utils/guard/GuestGuard.tsx
@@ -0,0 +1,22 @@
+import { frontend } from '@/routes/routes'
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+type GuestGuardProps = {
+  children: React.ReactNode
+}
+
+export const GuestGuard = ({ children }: GuestGuardProps) => {
+  const [mounted, setMounted] = useState(false)
+  const token = localStorage.getItem('token')
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (token) {
+      navigate(frontend.workspaces.index)
+    }
+    setMounted(true)
+  }, [token, navigate])
+
+  return <>{mounted && children}</>
+}
