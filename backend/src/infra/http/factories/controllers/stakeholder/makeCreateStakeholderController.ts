3e78e6248df093e84e0292824cb2542be1c8a1bd
chucrutes <tmelo387@gmail.com>
2023-10-29 11:06:48 -0300

    fix: minor changes

--- backend/src/infra/http/factories/controllers/stakeholder/makeCreateStakeholderController.ts
+++ backend/src/infra/http/factories/controllers/stakeholder/makeCreateStakeholderController.ts
@@ -1,6 +1,6 @@
-import { PrismaStakeholdersRepository } from '@/application/stakeholders/repositories/prisma/PrismaStakeholdersRepository'
-import { CreateStakeholder } from '@/application/stakeholders/use-cases/create-stakeholder/create-stakeholder'
-import { CreateStakeholderController } from '@/application/stakeholders/use-cases/create-stakeholder/create-stakeholder.controller'
+import { PrismaStakeholdersRepository } from '@/application/stakeholder/initiating/stakeholders/repositories/prisma/PrismaStakeholdersRepository'
+import { CreateStakeholder } from '@/application/stakeholder/initiating/stakeholders/use-cases/create-stakeholder/create-stakeholder'
+import { CreateStakeholderController } from '@/application/stakeholder/initiating/stakeholders/use-cases/create-stakeholder/create-stakeholder.controller'
 import { Controller } from '@/core/infra/controller'
 
 export function makeCreateStakeholderController(): Controller {
