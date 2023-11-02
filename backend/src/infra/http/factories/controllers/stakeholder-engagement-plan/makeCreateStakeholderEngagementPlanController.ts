52a9a8505619aa624f679e31d99cda8b2649a5d2
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2023-11-02 15:53:03 -0300

    fix: some imports

--- backend/src/infra/http/factories/controllers/stakeholder-engagement-plan/makeCreateStakeholderEngagementPlanController.ts
+++ backend/src/infra/http/factories/controllers/stakeholder-engagement-plan/makeCreateStakeholderEngagementPlanController.ts
@@ -1,6 +1,6 @@
-import { PrismaStakeholderEngagementPlansRepository } from '@/application/stakeholder_engagement_plans/repositories/prisma/PrismaStakeholderEngagementPlansRepository'
-import { CreateStakeholderEngagementPlan } from '@/application/stakeholder_engagement_plans/use-cases/create-stakeholder-engagement-plan/create-stakeholder-engagement-plan'
-import { CreateStakeholderEngagementPlanController } from '@/application/stakeholder_engagement_plans/use-cases/create-stakeholder-engagement-plan/create-stakeholder-engagement-plan.controller'
+import { PrismaStakeholderEngagementPlansRepository } from '@/application/processes/stakeholder_engagement_plans/repositories/prisma/PrismaStakeholderEngagementPlansRepository'
+import { CreateStakeholderEngagementPlan } from '@/application/processes/stakeholder_engagement_plans/use-cases/create-stakeholder-engagement-plan/create-stakeholder-engagement-plan'
+import { CreateStakeholderEngagementPlanController } from '@/application/processes/stakeholder_engagement_plans/use-cases/create-stakeholder-engagement-plan/create-stakeholder-engagement-plan.controller'
 import { Controller } from '@/core/infra/controller'
 
 export function makeCreateStakeholderEngagementPlanController(): Controller {
