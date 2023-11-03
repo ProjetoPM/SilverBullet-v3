b970352ced91cd5bbdc55ef6580f07a84efa962e
chucrutes <tmelo387@gmail.com>
2023-11-02 22:08:23 -0300

    fix: error name pattern

--- backend/src/application/workspaces/use-cases/create-workspace/errors/WorkspaceNotCreatedError.ts
+++ backend/src/application/workspaces/use-cases/create-workspace/errors/WorkspaceNotCreatedError.ts
@@ -3,6 +3,6 @@ import { t } from 'i18next'
 export class WorkspaceNotCreatedError extends Error {
   constructor() {
     super(t('workspace.not_created'))
-    this.name = 'WorkspaceNotCreated'
+    this.name = 'WorkspaceNotCreatedError'
   }
 }
