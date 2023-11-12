eb0c2a38ef05f0ee431e0a0dcf0947c1d84451a2
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2023-11-12 06:27:24 -0300

    fix: minor changes

--- frontend/src/pages/weekly-report/processes/actions/processes.files.tsx
+++ frontend/src/pages/weekly-report/processes/actions/processes.files.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@nextui-org/react'
 import { UploadIcon } from 'lucide-react'
-import { ChangeEvent, useEffect, useMemo } from 'react'
+import { ChangeEvent, useCallback, useMemo } from 'react'
 import { FieldArrayWithId } from 'react-hook-form'
 import { WeeklyReportDataWithId } from '../../weekly-report.schema'
 import { useProcesses } from '../context/ProcessProvider'
@@ -13,30 +13,31 @@ type UploadProcess = {
 
 export const FilesProcess = ({ field, index }: UploadProcess) => {
   const { form } = useProcesses()
-  const { images, addImages } = useWeeklyReport()
+  const { addImages } = useWeeklyReport()
 
   const filesFolder = useMemo(() => {
     return field.filesFolder ?? field.id
   }, [field])
 
-  useEffect(() => {
-    form.setValue(`processes.${index}.filesFolder`, filesFolder)
-  }, [form, filesFolder, index])
-
   const handleUpload = () => {
     document.getElementById(`processes.${index}.hidden`)?.click()
   }
 
-  const onChangeFile = (e: ChangeEvent<HTMLInputElement>) => {
-    const files = e.target.files
+  const onChangeFile = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      const files = e.target.files
+      console.log(filesFolder)
+      form.setValue(`processes.${index}.filesFolder`, filesFolder)
+      console.log('chamu essa caralha????')
 
-    if (!files) {
-      return
-    }
+      if (!files) {
+        return
+      }
 
-    addImages(filesFolder, files)
-    form.setValue(`processes.${index}.filesFolder`, filesFolder)
-  }
+      addImages(filesFolder, files)
+    },
+    [addImages, filesFolder, form, index]
+  )
 
   return (
     <>
@@ -45,12 +46,8 @@ export const FilesProcess = ({ field, index }: UploadProcess) => {
         type="file"
         multiple
         accept="image/*, application/pdf"
-        className="hidden"
-        {...form.register(`processes.${index}.filesFolder`)}
+        className="invisible absolute"
         onChange={onChangeFile}
-        onFocus={() =>
-          document.getElementById(`processes.${index}.filesFolder`)?.focus()
-        }
       />
       <Button
         id={`processes.${index}.filesFolder`}
@@ -62,15 +59,6 @@ export const FilesProcess = ({ field, index }: UploadProcess) => {
       >
         <UploadIcon className="w-5 h-5" />
       </Button>
-      {images
-        .filter((item) => item.filesFolder === filesFolder)
-        .map((item, index) => (
-          <div key={index}>
-            {item.files.map((file) => (
-              <div key={file.name}>{file.name}</div>
-            ))}
-          </div>
-        ))}
     </>
   )
 }
