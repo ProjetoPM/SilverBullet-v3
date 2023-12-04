93393f69cab583b90472ea98c6ceb7c19fdfdf39
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2023-12-04 12:51:26 -0300

    fix: add forwardRef in InputPassword

--- frontend/src/components/ui/input/InputPassword.tsx
+++ frontend/src/components/ui/input/InputPassword.tsx
@@ -1,5 +1,5 @@
 import { Input, InputProps } from '@nextui-org/react'
-import { useId, useState } from 'react'
+import { forwardRef, useId, useState } from 'react'
 import { TogglePasswordButton } from './TogglePasswordButton'
 
 type InputPasswordProps = InputProps & {
@@ -9,30 +9,33 @@ type InputPasswordProps = InputProps & {
   }
 }
 
-export const InputPassword = ({ icons, ...props }: InputPasswordProps) => {
-  const id = useId()
-  const [isVisible, setVisible] = useState(false)
+export const InputPassword = forwardRef<HTMLInputElement, InputPasswordProps>(
+  ({ icons, ...props }: InputPasswordProps, ref) => {
+    const id = useId()
+    const [isVisible, setVisible] = useState(false)
 
-  const toggleVisibility = () => {
-    setVisible((previous) => !previous)
+    const toggleVisibility = () => {
+      setVisible((previous) => !previous)
+    }
+
+    return (
+      <Input
+        id={`${id}-input-password`}
+        placeholder={'••••••••'}
+        type={isVisible ? 'text' : 'password'}
+        autoComplete="current-password"
+        classNames={{ inputWrapper: 'pr-1' }}
+        endContent={
+          <TogglePasswordButton
+            id={`${id}-toggle-password`}
+            isVisible={isVisible}
+            onPress={toggleVisibility}
+            icons={icons}
+          />
+        }
+        ref={ref}
+        {...props}
+      />
+    )
   }
-
-  return (
-    <Input
-      id={`${id}-input-password`}
-      placeholder={'••••••••'}
-      type={isVisible ? 'text' : 'password'}
-      autoComplete="current-password"
-      classNames={{ inputWrapper: 'pr-1' }}
-      endContent={
-        <TogglePasswordButton
-          id={`${id}-toggle-password`}
-          isVisible={isVisible}
-          onPress={toggleVisibility}
-          icons={icons}
-        />
-      }
-      {...props}
-    />
-  )
-}
+)
