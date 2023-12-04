bd42cc397fa8ce5f8a0e09f5c901e3f19f023eae
chucrutes <tmelo387@gmail.com>
2023-12-04 12:19:27 -0300

    feat: change password

--- backend/src/infra/http/factories/controllers/user/makeChangePasswordController.ts
+++ backend/src/infra/http/factories/controllers/user/makeChangePasswordController.ts
@@ -1,18 +1,18 @@
 import { PrismaUsersRepository } from '@/application/users/repositories/prisma/PrismaUsersRepository'
 import { ValidatorCompositor } from '@/infra/validation/ValidatorCompositor'
 import { Controller } from '@/core/infra/controller'
-import { ResetPassword } from '@/application/users/use-cases/reset-password/reset-password'
-import { ResetPasswordController } from '@/application/users/use-cases/reset-password/reset-password.controller'
 import { CompareFieldsValidator } from '@/infra/validation/CompareFieldsValidator'
+import { ChangePassword } from '@/application/users/use-cases/change-password/change-password'
+import { ChangePasswordController } from '@/application/users/use-cases/change-password/change-password.controller'
 
 type ComparableFields = {
   password: string
   confirmPassword: string
 }
 
-export function makeResetPasswordController(): Controller {
+export function makeChangePasswordController(): Controller {
   const prismaUsersRepository = new PrismaUsersRepository()
-  const resetPassword = new ResetPassword(prismaUsersRepository)
+  const resetPassword = new ChangePassword(prismaUsersRepository)
 
   const validator = new ValidatorCompositor<ComparableFields>([
     new CompareFieldsValidator({
@@ -22,5 +22,5 @@ export function makeResetPasswordController(): Controller {
     }),
   ])
 
-  return new ResetPasswordController(validator, resetPassword)
+  return new ChangePasswordController(validator, resetPassword)
 }
--- backend/src/infra/http/routes/user.routes.ts
+++ backend/src/infra/http/routes/user.routes.ts
@@ -4,9 +4,11 @@ import { makeForgotPasswordController } from '../factories/controllers/user/make
 import { makeResetPasswordController } from '../factories/controllers/user/makeResetPasswordController'
 import { adaptMiddleware } from '@/core/infra/adapters/express-middleware-adapter'
 import { makeEnsureAuthenticated } from '../factories/controllers/middlewares/makeEnsureAuthenticated'
+import { makeChangePasswordController } from '../factories/controllers/user/makeChangePasswordController'
 
 export const user = Router()
 
 user.post('/forgot-password', adaptRoute(makeForgotPasswordController()))
 user.post('/reset-password',adaptMiddleware(makeEnsureAuthenticated()), adaptRoute(makeResetPasswordController()))
+user.post('/change-password',adaptMiddleware(makeEnsureAuthenticated()), adaptRoute(makeChangePasswordController()))
 
