41900daacac0da104617d33da8d6b9c1012002fa
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2024-01-02 04:05:26 -0300

    chore: change vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,23 +1,13 @@
-import react from '@vitejs/plugin-react'
 import path from 'path'
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import { compression } from 'vite-plugin-compression2'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    compression(),
-    compression({
-      algorithm: 'brotliCompress',
-      exclude: [/\.(br)$ /, /\.(gz)$/]
-    })
-  ],
+  plugins: [react(), tsconfigPaths()],
   server: {
     port: 5500
   },
-  root: './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
