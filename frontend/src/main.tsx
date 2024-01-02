73f90f5f9e6165432f77398fb1d6e47172c73df2
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2024-01-02 03:06:52 -0300

    fix: minor changes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,17 +2,17 @@ import '@smastrom/react-rating/style.css'
 import './i18n'
 import './index.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
-import { QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router-dom'
 import { Loading } from './components/Loading'
 import { ToastSonner } from './components/Toast/ToastSonner'
 import { ThemeProvider } from './contexts/theme-provider'
 import { queryClient } from './lib/react-query'
 import { router } from './routes'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
